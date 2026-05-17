304f45a16dd5b0e737a5c596e8952b113ba3be00
Brian McCarthy <briansmc@gmail.com>
2026-05-16 22:26:50 -0400

    feat(sentiment): Import natural language processing package
    This commit imports the 'natural' npm package into the sentiment analysis module. This is a prerequisite for implementing sentiment analysis features in the application.

---
+++ sentiment/index.js
@@ -0,0 +1,9 @@
+/* written by Brian McCarthy */
+import natural from "natural";
+
+// Task 8: index.js imports natural package
+console.log("Sentiment analysis module loading...");
+const naturalPackage = natural;
+console.log("Natural package imported successfully in sentiment/index.js");
+
+export default naturalPackage;
